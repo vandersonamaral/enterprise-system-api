1c51b7a4ded59daf457f55b2e046a7166dba609c
vandersonamaral <vandersonamaral27@gmail.com>
2024-12-12 10:54:16 -0300

    fix: change employee ID type in RequestEmployee validation rules.

--- src/main/java/com/companies/enterprise/validation/RequestEmployee.java
+++ src/main/java/com/companies/enterprise/validation/RequestEmployee.java
@@ -4,10 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record RequestEmployee(
 
-        long id,
+        UUID id,
 
         @NotBlank
         String name,
