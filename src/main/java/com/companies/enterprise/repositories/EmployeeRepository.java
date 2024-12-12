7a985782f944bf158e856687eec972513a28081f
vandersonamaral <vandersonamaral27@gmail.com>
2024-12-12 10:52:03 -0300

    fix: optimize EmployeeRepository queries.

--- src/main/java/com/companies/enterprise/repositories/EmployeeRepository.java
+++ src/main/java/com/companies/enterprise/repositories/EmployeeRepository.java
@@ -3,5 +3,7 @@ package com.companies.enterprise.repositories;
 import com.companies.enterprise.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import java.util.UUID;
+
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
 }
