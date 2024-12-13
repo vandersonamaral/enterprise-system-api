49478a3201c1585ca62e4ed3316de172be3be304
vandersonamaral <vandersonamaral27@gmail.com>
2024-12-13 15:14:26 -0300

    feat: Refactor EmployeeService for better employee validation

--- src/main/java/com/companies/enterprise/services/EmployeeService.java
+++ src/main/java/com/companies/enterprise/services/EmployeeService.java
@@ -1,4 +1,36 @@
 package com.companies.enterprise.services;
 
+import com.companies.enterprise.entities.Address;
+import com.companies.enterprise.repositories.AddressRepository;
+import com.companies.enterprise.repositories.EmployeeRepository;
+import com.companies.enterprise.validation.RequestEmployee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class EmployeeService {
+
+
+        @Autowired
+        private AddressRepository addressRepository;
+        @Autowired
+        private EmployeeRepository employeeRepository;
+
+        public void validateEmployeeData(RequestEmployee data) {
+            Optional<Address> optionalAddress = addressRepository.findById(data.address_id());
+            if (optionalAddress.isEmpty()) {
+                throw new IllegalArgumentException("Endereço não encontrado");
+            }
+
+            if (data.supervisor_id() != null && data.supervisor_id().equals(data.id())) {
+                throw new IllegalArgumentException("O funcionário não pode ser supervisor de si mesmo");
+            }
+
+            if (data.supervisor_id() != null && employeeRepository.findById(data.supervisor_id()).isEmpty()) {
+                throw new IllegalArgumentException("Supervisor não encontrado");
+            }
+        }
 }
+
